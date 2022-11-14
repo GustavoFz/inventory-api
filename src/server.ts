1ea74936a688d5e66e19d3f0d7f1356d101c0d4d
Gustavo <gustavofrancojp@gmail.com>
2022-11-14 17:52:22 -0400

    added model refreshToken

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,16 @@
-
+require('dotenv').config();
 import cors from 'cors';
 import express from 'express';
 import morgan from 'morgan';
-import brandRouter from './routes/BrandRouter';
+
+import authMiddleware from './middlewares/authMiddleware';
+import sessionRouter from './routes/authRouter';
+import brandRouter from './routes/brandRouter';
 import groupRouter from './routes/groupRouter';
 import productRouter from './routes/productRoutes';
 import subgroupRouter from './routes/subgroupRouter';
 import transactionRouter from './routes/transactionRouter';
+import userRouter from './routes/userRouter';
 
 const app = express();
 
@@ -15,6 +19,14 @@ app.use(cors())
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// Public routes
+app.use(sessionRouter)
+
+// Middlewares
+app.use(authMiddleware)
+
+// Private routes
+app.use(userRouter)
 app.use(transactionRouter)
 app.use(productRouter)
 app.use(groupRouter)
